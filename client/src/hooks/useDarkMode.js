98f16a90616fb37cf36b02c7a76620d0f6759eec
raj u <r@Rs-MacBook-Air.local>
2020-02-14 11:19:09 -0800

    mvp and stretch

---
+++ client/src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import { useLocalStorage } from "./useLocalStorage";
+import { useEffect } from "react";
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
+  useEffect(() => {
+    
+    if (!darkMode) {
+      document.querySelector("body").classList.add("dark-mode");
+    } else {
+      document.querySelector("body").classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+  return [darkMode, setDarkMode];
+};
